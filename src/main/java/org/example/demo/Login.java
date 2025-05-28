17c39945f580440cab75d7d421203a9e45a28a54
tabris <3146463122@qq.com>
2025-05-28 10:56:49 +0800

    222

---
+++ src/main/java/org/example/demo/Login.java
@@ -0,0 +1,27 @@
+package org.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Login extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 420);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}

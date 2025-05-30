fd4ea30a57e5f3e71fd362137fe0c8fa7120fb75
tabris <3146463122@qq.com>
2025-05-31 07:18:42 +0800

    mysql-connector需要手动导入的问题已经解决

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,14 @@
-module org.demo {
+module front.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
-    requires jdk.compiler;
+    requires jdk.compiler;// Java 标准 SQL 模块
+    requires mysql.connector.java;
 
     opens front.Views to javafx.fxml;
     exports front.Views;
     exports front.Controller;
     opens front.Controller to javafx.fxml;
 }
+
